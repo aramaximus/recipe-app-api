b9b5b5ae4d494e5a3edf3d77b7e9af3b01921638
Arun Arora <62276910+aramaximus@users.noreply.github.com>
2022-04-27 15:58:32 +0530

    Added custom recipe derail view

--- recipe/views.py
+++ recipe/views.py
@@ -41,3 +41,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Return Recipies for current authenticated user only"""
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
